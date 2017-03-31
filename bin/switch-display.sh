42e991804e6e2ca280c1177889179a53c98f1994
Ertugrul Söylemez <esz@posteo.de>
2017-03-31 21:53:34 +0200

    X settings

--- bin/switch-display.sh
+++ bin/switch-display.sh
@@ -10,7 +10,7 @@ for n in $*; do
                 --output eDP1 --scale 1x1 \
                 --output HDMI1 --auto --scale-from 1366x768 --same-as eDP1 \
                 --output VGA1 --off
-            xmodmap_defs+=("-DDEIMOS_MOBILE")
+            xmodmap_defs+=("-DMOBILE")
             ;;
 
         deimos-home)
@@ -18,7 +18,6 @@ for n in $*; do
                 --output eDP1 --off \
                 --output HDMI1 --off \
                 --output VGA1 --auto --primary
-            xmodmap_defs+=("-DDEIMOS_HOME")
             ;;
 
         deimos-mobile)
@@ -26,16 +25,63 @@ for n in $*; do
                 --output eDP1 --auto --primary \
                 --output HDMI1 --off \
                 --output VGA1 --off
-            xmodmap_defs+=("-DDEIMOS_MOBILE")
+            xmodmap_defs+=("-DMOBILE")
+            ;;
+
+        phobos-beamer)
+            xrandr \
+                --output eDP1 --auto \
+                --output DP1 --auto --same-as eDP1 --scale-from 1920x1080 \
+                --output DP2 --off \
+                --output HDMI1 --off \
+                --output HDMI2 --off \
+                --output VIRTUAL1 --off
+            xmodmap_defs+=("-DMOBILE")
+            ;;
+
+        phobos-home)
+            xrandr \
+                --output eDP1 --auto \
+                --output DP1 --auto --left-of eDP1 \
+                --output DP2 --off \
+                --output HDMI1 --off \
+                --output HDMI2 --off \
+                --output VIRTUAL1 --off
+            ;;
+
+        phobos-mobile)
+            xrandr \
+                --output eDP1 --auto \
+                --output DP1 --off \
+                --output DP2 --off \
+                --output HDMI1 --off \
+                --output HDMI2 --off \
+                --output VIRTUAL1 --off
+            xmodmap_defs+=("-DMOBILE")
+            ;;
+
+        phobos-single)
+            xrandr \
+                --output eDP1 --auto \
+                --output DP1 --off \
+                --output DP2 --off \
+                --output HDMI1 --off \
+                --output HDMI2 --off \
+                --output VIRTUAL1 --off
             ;;
     esac
 done
 
-nvidia-settings -l
-wacom.sh
-cpp -P $xmodmap_defs ~/cfg/my/xmodmap | xmodmap -
-xrdb -merge ~/cfg/my/xresources
-xset r rate 250 30
-xsetroot -cursor_name left_ptr
+# nvidia-settings -l &
+wacom.sh &
+cpp -P $xmodmap_defs ~/cfg/my/xmodmap | xmodmap - &
+xrdb -merge ~/cfg/my/xresources &
+xset r rate 250 30 &
+xsetroot -cursor_name left_ptr &
 
-feh --bg-fill ~/gfx/bgr/current.jpg
+bgrscript=~/.fehbg
+for n in $*; do
+    fn=~/gfx/bgr/configs/$n.sh
+    [[ -f $fn && -x $fn ]] && bgrscript=$fn
+done
+$bgrscript &
